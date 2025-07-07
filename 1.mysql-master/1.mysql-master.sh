40b459f057b52b0c7994fab80a4c9ec9a645dc14
Semen Smolyankin <zibo99@gmail.com>
2025-07-07 20:25:38 +0500

    mysql root pass

--- 1.mysql-master/1.mysql-master.sh
+++ 1.mysql-master/1.mysql-master.sh
@@ -9,11 +9,11 @@ systemctl restart mysql
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'caching_sha2_password' BY '$ROOT_PASS';"
 
-mysql -e "CREATE USER IF NOT EXISTS repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY '$REPL_PASS';"
-mysql -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p $ROOT_PASS -e "CREATE USER IF NOT EXISTS repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY '$REPL_PASS';"
+mysql -u root -p $ROOT_PASS -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';"
+mysql -u root -p $ROOT_PASS -e "FLUSH PRIVILEGES;"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE TABLE IF NOT EXISTS requests (
+mysql -u root -p $ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -u root -p $ROOT_PASS -e "CREATE TABLE IF NOT EXISTS requests (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     ip VARCHAR(45) NOT NULL);"
