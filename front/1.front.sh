d0242c1194eba2f4e4b83d641a0e9fce258be0aa
Semen Smolyankin <zibo99@gmail.com>
2025-07-09 22:52:23 +0500

    fix front

--- front/1.front.sh
+++ front/1.front.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Копирование настроек nginx/nginx.conf"
-cp /home/ad/otus/4.front/nginx.conf /etc/nginx/nginx.conf
+cp /home/ad/otus/front/nginx.conf /etc/nginx/nginx.conf
 #rm -f /etc/nginx/sites-enabled/*
 echo "Копирование настроек nginx/sites-enabled/default"
-cp /home/ad/otus/4.front/default /etc/nginx/sites-enabled/default
+cp /home/ad/otus/front/default /etc/nginx/sites-enabled/default
 
 echo "Перезапуск nginx"
 systemctl restart nginx
