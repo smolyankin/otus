5af478aeda87f0a6c54bff040e1567d4087d9e5a
Semen Smolyankin <zibo99@gmail.com>
2025-07-12 22:55:20 +0500

    remove git backup

--- mysql/master/2.mysql-master-restore.sh
+++ mysql/master/2.mysql-master-restore.sh
@@ -2,11 +2,6 @@
 
 DB_NAME="otus"
 
-echo "Пул бэкапа из репозитория"
-cd /home/ad/otus
-git pull
-
-
 echo "Удаление базы данных"
 mysql -uroot -e "DROP DATABASE IF EXISTS $DB_NAME;"
 
@@ -14,6 +9,6 @@ echo "Создание базы данных"
 mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 echo "Восстановление базы данных"
-mysql -uroot otus < /home/ad/otus/mysql/backup.sql
+mysql -uroot otus < /home/ad/backup.sql
 
 echo "Восстановление базы данных успешно завершено"
--- mysql/slave/2.mysql-slave-backup.sh
+++ mysql/slave/2.mysql-slave-backup.sh
@@ -4,13 +4,7 @@ echo "Остановка репликации"
 mysql -uroot -e "STOP REPLICA;"
 
 echo "Бэкап БД otus"
-mysqldump -uroot -e --databases otus > /home/ad/otus/mysql/backup.sql
-
-echo "Пуш бэкапа в репозиторий"
-git config --global user.email 'zibo99@gmail.com'
-git config --global user.name 'smolyankin'
-cd /home/ad/otus
-git add . && git commit -m 'Database backup' && git push origin main
+mysqldump -uroot -e --databases otus > /home/ad/backup.sql
 
 echo "Возобновление репликации"
 mysql -uroot -e "START REPLICA;"
